5cb47bf7d62262c721a67d750bd4f3aa29f08f28
juanfeoru <felipeortega383@gmail.com>
2026-08-17 17:15:27 -0500

    feat: add color format selection

--- js/main.js
+++ js/main.js
@@ -6,11 +6,44 @@ import {
 } from "./modules/color-converter.js";
 import { generateRandomColor } from "./modules/color-generator.js";
 
-const color = generateRandomColor();
+const generateButton = document.querySelector(".color-generator__generate");
+const colorDisplay = document.querySelector(".color-generator__swatch");
+const colorValue = document.querySelector(".color-generator__hex");
+const formatButtons = document.querySelectorAll(".color-generator__format");
 
-const hex = rgbToHex(color);
-const hsl = rgbToHsl(color);
+let currentColor = null;
 
-console.log(hex);
-console.log(rgbToString(color));
-console.log(hslToString(hsl));
+function updateColor() {
+  currentColor = generateRandomColor();
+
+  const hex = rgbToHex(currentColor);
+
+  colorDisplay.style.background = hex;
+  colorValue.textContent = hex;
+}
+
+function setActiveFormat(selectedButton) {
+  formatButtons.forEach((button) => {
+    button.classList.remove("color-generator__format--active");
+  });
+
+  selectedButton.classList.add("color-generator__format--active");
+}
+
+formatButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const format = button.dataset.format;
+
+    setActiveFormat(button);
+
+    if (format === "hex") {
+      colorValue.textContent = rgbToHex(currentColor);
+    } else if (format === "rgb") {
+      colorValue.textContent = rgbToString(currentColor);
+    } else if (format === "hsl") {
+      colorValue.textContent = hslToString(rgbToHsl(currentColor));
+    }
+  });
+});
+
+generateButton.addEventListener("click", updateColor);
