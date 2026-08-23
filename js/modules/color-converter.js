9eb868dcdf8373a6daee54741132e48169fb82d0
juanfeoru <felipeortega383@gmail.com>
2026-08-23 16:57:47 -0500

    feat: add HSL to RGB conversion

--- js/modules/color-converter.js
+++ js/modules/color-converter.js
@@ -42,6 +42,52 @@ export function rgbToHsl(color) {
   };
 }
 
+export function hslToRgb(color) {
+  const h = color.h / 360;
+  const s = color.s / 100;
+  const l = color.l / 100;
+
+  const c = (1 - Math.abs(2 * l - 1)) * s;
+  const x = c * (1 - Math.abs(((h * 6) % 2) - 1));
+  const m = l - c / 2;
+
+  let r;
+  let g;
+  let b;
+
+  if (h < 1 / 6) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (h < 2 / 6) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (h < 3 / 6) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (h < 4 / 6) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (h < 5 / 6) {
+    r = x;
+    g = 0;
+    b = c;
+  } else {
+    r = c;
+    g = 0;
+    b = x;
+  }
+
+  return {
+    r: Math.round((r + m) * 255),
+    g: Math.round((g + m) * 255),
+    b: Math.round((b + m) * 255),
+  };
+}
+
 export function rgbToString(color) {
   return `rgb(${color.r}, ${color.g}, ${color.b})`;
 }
